29440abbb09e30f8685032351fbc78e42cff7150
Risna Riha <risnariha994563@gmail.com>
2026-04-26 14:37:08 +0530

    Initial commit

---
+++ src/test/java/com/convergence/cms/CmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.convergence.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

4239d8ccedd6e3ecb0f6220506166ba106fb5c36
Risna Riha <risnariha994563@gmail.com>
2026-04-26 15:46:59 +0530

    city file done

--- src/main/java/com/convergence/cms/entity/City.java
+++ src/main/java/com/convergence/cms/entity/City.java
@@ -1,4 +1,16 @@
 package com.convergence.cms.entity;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
 public class City {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
 }

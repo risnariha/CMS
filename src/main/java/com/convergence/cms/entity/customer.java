9c8838249364713b3b3387b3d0de4b3d713874f6
Risna Riha <risnariha994563@gmail.com>
2026-04-26 15:42:42 +0530

    customer.java created

---
+++ src/main/java/com/convergence/cms/entity/customer.java
@@ -0,0 +1,33 @@
+package com.convergence.cms.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.*;
+
+@Entity
+@Data
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Temporal(TemporalType.DATE)
+    private Date dob;
+
+    @Column(unique = true)
+    private String nic;
+
+    @ElementCollection
+    private List<String> mobileNumbers = new ArrayList<>();
+
+    @ManyToMany
+    private List<Customer> familyMembers = new ArrayList<>();
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Address> addresses = new ArrayList<>();
+}

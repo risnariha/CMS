7545063377cd780f7801421f17094675288a3370
Risna Riha <risnariha994563@gmail.com>
2026-04-26 15:47:44 +0530

    country file done

--- src/main/java/com/convergence/cms/entity/Country.java
+++ src/main/java/com/convergence/cms/entity/Country.java
@@ -1,4 +1,16 @@
 package com.convergence.cms.entity;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
 public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
 }
